e89d50ccc2019d0c9ff15956bfb90a63ead1c8e7
pawelmat142 <pawelmat142@gmail.com>
2025-01-03 13:28:04 +0100

    fake data generator - artists

--- services/src/data-generator/data-generator.service.ts
+++ services/src/data-generator/data-generator.service.ts
@@ -3,7 +3,7 @@ import { ProfileService } from '../profile/profile.service';
 import { ProfileEmailService } from '../profile/profile-email.service';
 import { Role } from '../profile/model/role';
 import { ManagerData } from '../profile/model/profile-interfaces';
-import { Country } from '../artist/model/artist.model';
+import { ArtistLabel, ArtistMedia, ArtistStyle, Country } from '../artist/model/artist.model';
 import { JwtPayload } from '../profile/auth/jwt-strategy';
 import { Profile } from '../profile/model/profile.model';
 import { Util } from '../global/utils/util';
@@ -12,6 +12,9 @@ import { ArtistForm, SelectorItem } from '../artist/artist.controller';
 import { ArtistViewDto } from '../artist/model/artist-view.dto';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
+import { ARTIST_IMAGES } from './data/artist-images';
+import { COUNTRIES } from './data/countries';
+import { ARTIST_LABEL, ARTIST_MEDIA, ARTIST_STYLE } from './data/artist-info';
 
 @Injectable()
 export class DataGeneratorService {
@@ -35,6 +38,8 @@ export class DataGeneratorService {
     readonly ARTIST_EMAIL = 'artist@test.com'
     ARTIST_SIGNATURE = 'ARTIST_SIGNATURE'
 
+    ARTISTS: ArtistViewDto[] = []
+
     readonly MANAGER_2 = 'SynthSphere Booking'
     readonly MANAGER_2_EMAIL = 'manager@synth-sphere-booking.com'
 
@@ -51,6 +56,7 @@ export class DataGeneratorService {
 
     async dataGenerator() {
         await this.cleanDatabase()
+
         await this.generateManagers()
         await this.generateArtists()
     }
@@ -62,19 +68,6 @@ export class DataGeneratorService {
           await collection.deleteMany({});
           console.log(`Cleaned collection: ${collectionName}`);
         }
-    }
-
-
-    private async generateArtists() {
-        const artistProfile = await this.generateArtist('Neon Reverie', this.ARTIST_EMAIL)
-        this.ARTIST_SIGNATURE = artistProfile.signature
-        // const artistProfile1 = await this.generateArtist('Pulse Machine')
-        // const artistProfile2 = await this.generateArtist('Echo Synthesis')
-        // const artistProfile3 = await this.generateArtist('Synthwave Odyssey')
-
-
-        // this.artistService.createArtist(this.MANAGER_UID)
-
     }
 
     private async generateManagers() {
@@ -95,8 +88,51 @@ export class DataGeneratorService {
         await this.generateManager(this.MANAGER_2, this.MANAGER_2_EMAIL)
     }
 
-    private async generateArtist(name: string, email?: string): Promise<ArtistViewDto> {
-        const mail = email ? email : Util.toKebabCase(name)
+
+    // export const artistMediaCodes = [
+    //     '', 
+    //     'facebook', 
+    //     'instagram', 
+    //     'soundcloud', 
+    //     'bandcamp',
+    //     'spotify',
+    //     'you_tube',
+    //     'website'
+    // ] as const
+
+    private async generateArtists() {
+        let i = 0;
+        const artistProfile = await this.generateArtist(
+            'Neon Reverie', 
+            this.ARTIST_EMAIL, 0, 
+            [ARTIST_LABEL[0], ARTIST_LABEL[1]], //0-4, 
+            [ARTIST_STYLE[0], ARTIST_STYLE[1], ARTIST_STYLE[2]], //0-8
+            [ ARTIST_MEDIA.instagram, ARTIST_MEDIA.soundcloud, ARTIST_MEDIA.spotify ]
+        )
+        this.ARTIST_SIGNATURE = artistProfile.signature
+        const artistProfile2 = await this.generateArtist('Pulse Machine', '', 1, [ARTIST_LABEL[2]], [ARTIST_STYLE[3], ARTIST_STYLE[2]], [ARTIST_MEDIA.instagram, ARTIST_MEDIA.bandcamp, ARTIST_MEDIA.spotify])
+        const artistProfile3 = await this.generateArtist('Echo Synthesis', '', 2, [ARTIST_LABEL[3], ARTIST_LABEL[4]], [ARTIST_STYLE[3], ARTIST_STYLE[4]], [ARTIST_MEDIA.soundcloud, ARTIST_MEDIA.bandcamp,ARTIST_MEDIA.instagram, ARTIST_MEDIA.you_tube])
+        const artistProfile4 = await this.generateArtist('Synthwave Odyssey', '', 3, [ARTIST_LABEL[0]], [ARTIST_STYLE[3], ARTIST_STYLE[2]], [ARTIST_MEDIA.instagram, ARTIST_MEDIA.spotify, ARTIST_MEDIA.facebook, ARTIST_MEDIA.bandcamp])
+        
+        const artistProfile5 = await this.generateArtist('Infinity Circuit', '', 4, [], [ARTIST_STYLE[3], ARTIST_STYLE[2]], [ARTIST_MEDIA.instagram, ARTIST_MEDIA.spotify, ARTIST_MEDIA.facebook, ARTIST_MEDIA.bandcamp])
+        const artistProfile6 = await this.generateArtist('Arctic Bass', '', 5, [ARTIST_LABEL[1]], [ARTIST_STYLE[3], ARTIST_STYLE[2]], [ARTIST_MEDIA.instagram, ARTIST_MEDIA.you_tube,  ARTIST_MEDIA.spotify, ARTIST_MEDIA.soundcloud])
+        const artistProfile7 = await this.generateArtist('Quantum Beats', '', 6, [ARTIST_LABEL[2]], [ARTIST_STYLE[8], ARTIST_STYLE[1],ARTIST_STYLE[7]], [ARTIST_MEDIA.instagram, ARTIST_MEDIA.spotify, ARTIST_MEDIA.soundcloud])
+        const artistProfile8 = await this.generateArtist('Crystal Oscillator', '', 7, [ARTIST_LABEL[3], ARTIST_LABEL[1]], [ARTIST_STYLE[4], ARTIST_STYLE[5]], [ARTIST_MEDIA.instagram, ARTIST_MEDIA.spotify, ARTIST_MEDIA.facebook, ARTIST_MEDIA.you_tube])
+        const artistProfile9 = await this.generateArtist('Stellar Waves', '', 8, [ARTIST_LABEL[4]], [ARTIST_STYLE[8], ARTIST_STYLE[0], ARTIST_STYLE[6], ARTIST_STYLE[7]], [ARTIST_MEDIA.instagram, ARTIST_MEDIA.bandcamp, ARTIST_MEDIA.facebook])
+        
+        this.ARTISTS.push(artistProfile)
+        this.ARTISTS.push(artistProfile2)
+        this.ARTISTS.push(artistProfile3)
+        this.ARTISTS.push(artistProfile4)
+        this.ARTISTS.push(artistProfile5)
+        this.ARTISTS.push(artistProfile6)
+        this.ARTISTS.push(artistProfile7)
+        this.ARTISTS.push(artistProfile8)
+        this.ARTISTS.push(artistProfile9)
+    }
+
+    private async generateArtist(name: string, email: string, index: number, labels?: ArtistLabel[], styles?: ArtistStyle[], medias?: ArtistMedia[]): Promise<ArtistViewDto> {
+        const mail = email ? email : `${Util.toKebabCase(name)}@test.com`
         const user = await this.profileEmailService.createProfile({
             name: name,
             role: { code: Role.ARTIST, name: Role.ARTIST },
@@ -120,9 +156,35 @@ export class DataGeneratorService {
         const artist = await this.artistService.createArtist(artistForm, { uid: user.uid } as JwtPayload )
         this.logger.log(`Artist created ${artist.name}`)
 
+        // 
+        const images = ARTIST_IMAGES[index] || ARTIST_IMAGES[0]
+        if (images) {
+            artist.images = images
+        }
+        artist.country = COUNTRIES[index] || COUNTRIES[0]
+        artist.bio = this.getArtistBio(artist.name)
+        artist.labels = labels
+        artist.styles = styles
+        artist.medias = medias
+        const artistProfile = { uid: user.uid, artistSignature: artist.signature } as JwtPayload
+        const managerProfile = { uid: this.MANAGER_UID } as JwtPayload
+        await this.artistService.updateArtistView(artist, artistProfile )
 
+        await this.artistService.putManagementNotes({
+            managmentNotes: this.getArtistManagementNotes(index%2 + 1),
+            artistSignture: artist.signature
+        }, managerProfile)
 
+        await this.artistService.setStatus('ACTIVE', artist.signature, managerProfile)
         return artist
+    }
+
+    private getArtistBio(name: string): string {
+        return this.getRandomParagraphs(this.paragraphs).join('\n\n')
+    }
+
+    private getArtistManagementNotes(paragraphs: number): string {
+        return this.getRandomParagraphs(this.paragraphs, paragraphs).join('\n\n')
     }
 
     private async generateManager(name: string, email: string, managerData?: ManagerData): Promise<Profile> {
@@ -138,4 +200,22 @@ export class DataGeneratorService {
         this.logger.log(`MANAGER ${name} generated`)
         return user
     }
+
+    private getRandomParagraphs(arr: string[], paragraphs?: number) {
+        const numElements = paragraphs || Math.floor(Math.random() * (arr.length - 1)) + 1; // Random number between 1 and arr.length - 1
+        const shuffledArr = [...arr].sort(() => Math.random() - 0.5); // Shuffle the array
+        return shuffledArr.slice(0, numElements);
+    }
+
+    readonly paragraphs: string[] = [
+        `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
+        `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+        `It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+        `It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+        `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.`,
+        `The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.`,
+        `Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.`,
+        `Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`,
+        `The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`
+    ]
 }
