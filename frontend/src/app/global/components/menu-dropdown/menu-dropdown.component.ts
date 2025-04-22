f4e9816416276912b17f890d6f7442877a534e74
pawelmat142 <pawelmat142@gmail.com>
2025-04-22 19:17:38 +0200

    prevent some console error

--- frontend/src/app/global/components/menu-dropdown/menu-dropdown.component.ts
+++ frontend/src/app/global/components/menu-dropdown/menu-dropdown.component.ts
@@ -2,13 +2,15 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { IconButtonComponent } from '../icon-button/icon-button.component';
 import { Menu, MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu-dropdown',
   standalone: true,
   imports: [
     IconButtonComponent,
-    MenuModule
+    MenuModule,
+    CommonModule
   ],
   templateUrl: './menu-dropdown.component.html',
   styleUrl: './menu-dropdown.component.scss'
@@ -22,9 +24,15 @@ export class MenuDropdownComponent {
 
   @ViewChild('menuDropdown') menuDropdownRef?: ElementRef
   
-
   _toggle(event: Event) {
     this.menuRef?.toggle(event)
+  }
+  apendTo?: any
+  
+  ngOnInit() {
+    setTimeout(() => {
+      this.apendTo = this.menuDropdownRef?.nativeElement
+    })
   }
 
 }
